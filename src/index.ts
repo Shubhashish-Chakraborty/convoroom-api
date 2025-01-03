a701c18bdcc6f5c2a1cfa0c4a6d8d73e2201f01a
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-01-03 23:31:05 +0530

    started with a simple chatapp code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("HEY THERE I AM A WEBSOCKET BACKEND FOR A CHAT APP");
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({port:3000});
+
+let userCount = 0;
+let allSocket = [];
+
+
+wss.on("connection" , (socket) => {
+    allSocket.push(socket);
+
+    userCount += 1;
+    console.log(`User:${userCount} Connected to the WebSocket Server!`);
+
+    socket.on("message" , (e) => {
+        // console.log(`${e.toString()}: => message sent from the server!`);        
+        // we've to send message to all the users/sockets
+        allSocket.forEach((s) => {
+            s.send(`${e.toString()}`)
+        })
+        
+    })
+            
+})
