f49d7cd41254d658dee6a482102232a08a33edd2
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-01-06 14:40:27 +0530

    made the ws server more working

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,98 @@
 import { WebSocketServer , WebSocket } from "ws";
 const wss = new WebSocketServer({port:3000});
 
+// Client Message Schemas:
+
+// join the room:
+// {
+// 	type: "join", // Message type is join
+// 	payload: {
+// 		"roomId" : "123123",
+// 		"name" : "shubh"	
+// 	}
+// }
+
+// send a message:
+// {
+// 	type : "chat",
+// 	payload: {
+// 		"textMessage" : "Hii there, how's everyone doing!"
+// 	}	
+// }
+
+
+
+// Server Message Schemas:
+
+// {
+// 	type : "chat",
+// 	payload: {
+// 		"message" : "Hii there, how's everyone doing!",
+// 		"sender" : "shubh"
+// 	}
+// }
 
 interface User {
     socket: WebSocket,
-    room: string
+    room: string,
+    name: string
 }
 
 let allSocket: User[] = [];
 
+let userCount = 0;
 
 wss.on("connection" , (socket) => {
 
-    console.log("SOMEONE CONNECTED TO THE SERVER");
+    userCount += 1;
+    console.log(`User Present in the Server: ${userCount}`);
     
     socket.on("message" , (message) => {
-        const parsedMessage = JSON.parse(message as unknown as string); // or tsignore as your wish
-
+        // or tsignore as your wish
+        const parsedMessage = JSON.parse(message as unknown as string); // Message that came from the client "{...}"
         
+        // if someone wants to join a room!
         if (parsedMessage.type === "join") { // if the person wants to join the room then you'll push to the allSocket
-            console.log(`${parsedMessage.payload.name} Joined the room: ${parsedMessage.payload.roomId}`);
-            
+        
             allSocket.push({
                 socket,
-                room: parsedMessage.payload.roomId
+                room: parsedMessage.payload.roomId,
+                name: parsedMessage.payload.name
+            })
+            
+            // finding the room where the current user joined, and letting the members of THAT room know that someone joined!
+
+            const currentUserRoom = allSocket.find((x) => x.socket == socket)?.room;
+            allSocket.forEach((user) => {
+                if (user.room == currentUserRoom) {
+                    user.socket.send(`${parsedMessage.payload.name} Joined room: ${parsedMessage.payload.roomId}`);
+                }
             })
         }
+        
+        // now the user has joined there room and they want to chat!, within there members:
         if (parsedMessage.type === "chat") {
             // console.log(`${parsedMessage.payload.name} Messaged: ${parsedMessage.payload.textMessage}`);
 
             const currentUserRoom = allSocket.find((x) => x.socket == socket)?.room;
+            const currentUserName = allSocket.find((x) => x.socket == socket)?.name;
 
             allSocket.forEach((userObj) => {
                 if (userObj.room == currentUserRoom) {
-                    userObj.socket.send(parsedMessage.payload.textMessage)
+                    userObj.socket.send(JSON.stringify({
+                        message: parsedMessage.payload.textMessage,
+                        name: currentUserName
+                    }))
+                    // userObj.socket.send(`${currentUserName} messaged: ${parsedMessage.payload.textMessage}`)
                 }
             })            
         }
 
+    })
+
+    socket.on("close" , () => {
+        userCount -= 1
+        console.log(`User disconnected. Users remaining: ${userCount}`);
     })
 
             
